729ef6f264d50d8c1509c34f9f8f1039fc207532
Henok Lamiso <epapalink68@gmail.com>
2022-08-11 15:10:28 -0600

    file imported

--- Implementation using Python/main.py
+++ Implementation using Python/main.py
@@ -1,33 +1,32 @@
 # call the implementation from Implementation using Python/RedBlackTree.py
 # Show menu for user and get user input
-def menu():
-    print('1. Insert')
-    print('2. Delete')
-    print('3. Search')
-    print('4. Print')
-    print('5. Exit')
-    return input('Enter your choice: ')
 
-# Insert a new node in the tree
-def insert(tree, key, value):
-    tree.insert(key, value)
-    print('Inserted ' + str(key) + ' ' + str(value))
+import RedBlackTree
 
-# Delete a node in the tree
-def delete(tree, key):
-    tree.delete(key)
-    print('Deleted ' + str(key))
+def main():
+    tree = RedBlackTree.RedBlackTree()
+    while True:
+        print("1. Insert")
+        print("2. Delete")
+        print("3. Search")
+        print("4. Print")
+        print("5. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            key = int(input("Enter key: "))
+            value = int(input("Enter value: "))
+            tree.insert(key, value)
+        elif choice == 2:
+            key = int(input("Enter key: "))
+            tree.delete(key)
+        elif choice == 3:
+            key = int(input("Enter key: "))
+            print(tree.search(key))
+        elif choice == 4:
+            print(tree)
+        elif choice == 5:
+            break
+        else:
+            print("Invalid choice")
 
-# Search for a node in the tree
-def search(tree, key):
-    node = tree.search(key)
-    if node == None:
-        print('Not found')
-    else:
-        print('Found ' + str(node.key) + ' ' + str(node.value))
-
-# Print the tree
-def print_tree(tree):
-    print(tree)
-
-# Main function
+            
